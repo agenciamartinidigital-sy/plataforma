bd0764d4a8a85548d7a36f5ba9e1c0df6db1e115
Luís Martini <agenciamartinidigital@gmail.com>
2026-05-15 10:29:47 -0300

    att

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 8080;
 
 app.set("view engine", "ejs");
 
 app.get("/:nome/:lang", (req, res) => {
-  const nome = req.params.nome;
-  const lang = req.params.lang;
-  const exibirMsg = true;
+  let nome = req.params.nome;
+  let lang = req.params.lang;
+  let exibirMsg = true;
   res.render("principal/perfil", {
     nome,
     lang,
     empresa: "Nexxum",
-    msg: exibirMsg,
-  });
-});
+    msg: exibirMsg
+  })
+})
 
-app.listen(port, () => {
+app.listen(8080, () => {
   console.log("App rodando!");
 });
