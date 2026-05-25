f95979c5c87f2ce0ce7cb34b6d3b609d8d4224a6
Luís Martini <agenciamartinidigital@gmail.com>
2026-05-25 16:47:31 -0300

    fix: use environment variables for database connection

--- database/database.js
+++ database/database.js
@@ -1,9 +1,15 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("guiaperguntas", "root", "luis140777", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = connection;
